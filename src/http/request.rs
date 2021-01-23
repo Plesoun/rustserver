4236edaa2c9be6c312cdc943b824e02928398616
Jakub Schier <jakub.schier@gmail.com>
2021-01-23 14:34:26 +0100

    way of extending functionality of alredy defined types

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,8 +1,40 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
     query_string: Option<String>, //can be None or some, it is a way to express absence of a value in a type-safe way (no no pointer exceptions)
     // use enums here instead of string
     method: Method,
+}
+
+impl Request {
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {}
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
+
+// Extending functionality of an existing modules
+
+trait Encrypt {
+    fn encrypt(&self) -> Self {
+        unimplemented!()
+    }
+}
+
+impl Encrypt for String {
+    fn encrypt(&self) -> Self {
+        unimplemented!()
+    }
+}
+
+impl Encrypt for &[u8] {
+    fn encrypt(&self) -> Self {
+        unimplemented!()
+    }
 }
