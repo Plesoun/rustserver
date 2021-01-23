fd95328b6b2f8583de629c01df5a0cb9b2f522e7
Jakub Schier <jakub.schier@gmail.com>
2021-01-23 11:29:01 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+
+    let string = String::from("127.0.0.1:8080");
+    // borrow the slice of an existing string allocated in memory
+    let string_slice = &string[10..];
+    dbg!(&string);
+    dbg!(string_slice);
+
+
+    //let server = Server::new("127.0.0.1:8080");
+    //server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+// Self and Server are interchangeable
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr
+        }
+    }
+
+    fn run(self) {
+
+    }
 }
