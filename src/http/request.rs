52355b8b7042afa9b4f13ad4ddbcbffd2db7e200
Jakub Schier <jakub.schier@gmail.com>
2021-01-23 15:26:24 +0100

    Implement ParseError directly for Utf8Error

--- src/http/request.rs
+++ src/http/request.rs
@@ -2,6 +2,7 @@ use std::str;
 use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
+use std::str::Utf8Error;
 use std::fmt::{Result as FmtResult, Display, Formatter, Debug};
 
 pub struct Request {
@@ -22,7 +23,7 @@ impl TryFrom<&[u8]> for Request {
         //}
 
         //or simply like so:
-        let request = str::from_utf8(buff).or(Err(ParseError::InvalidEncoding))?;
+        let request = str::from_utf8(buff)?;
         // the only difference is, when the error type is not matched to our ParseError, it tries to convert the error type
 
         unimplemented!()
@@ -56,6 +57,14 @@ impl ParseError {
             Self::InvalidProtocol => "InvalidProtocol",
             Self::InvalidMethod => "InvalidMethod",
         }
+    }
+}
+
+
+//to be able to do this: let request = str::from_utf8(buff)?;
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
