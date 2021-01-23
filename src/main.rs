57677e39f7095ba3de0d2b773b63efc0917757e9
Jakub Schier <jakub.schier@gmail.com>
2021-01-23 12:20:54 +0100

    inline modules

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,58 @@
+use server::Server;
+use http::request::Request;
+
 fn main() {
-
-    let get = Method::GET;
-    let delete = Method::DELETE;
-    let post = Method::POST;
-    let put = Method::PUT;
-
 
     let server = Server::new("127.0.0.1:8080".to_string()); //need to convert to_string, was a literal
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
+mod server {
 
-// Self and Server are interchangeable
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
+    pub struct Server {
+        addr: String,
+    }
+
+    // Self and Server are interchangeable
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self {
+                addr
+            }
+        }
+
+        pub fn run(self) {
+            println!("Listening on {}", self.addr);
         }
     }
+}
 
-    fn run(self) {
-        println!("Listening on {}", self.addr);
+mod http {
+    pub mod request {
+
+        use super::method::Method;
+        
+        pub struct Request {
+            path: String,
+            query_string: Option<String>, //can be None or some, it is a way to express absence of a value in a type-safe way (no no pointer exceptions)
+            // use enums here instead of string
+            method: Method,
+        }
     }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>, //can be None or some, it is a way to express absence of a value in a type-safe way (no no pointer exceptions)
-    // use enums here instead of string
-    method: Method,
-}
-
-// will be represented as ints in memory
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
+    pub mod method {
+        // will be represented as ints in memory
+        pub enum Method {
+            GET,
+            DELETE,
+            POST,
+            PUT,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
+    }
 }
 
 
