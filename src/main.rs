e9af1976c7076f59d82e4d65c8cdc1e65b3b9126
Jakub Schier <jakub.schier@gmail.com>
2021-01-23 11:54:55 +0100

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
 
-    let server = Server::new("127.0.0.1:8080");
+    let get = Method::GET;
+    let get = Method::DELETE;
+    let get = Method::POST;
+    let get = Method::PUT;
+
+    let server = Server::new("127.0.0.1:8080".to_string()); //need to convert to_string, was a literal
     server.run();
 }
 
@@ -17,6 +22,35 @@ impl Server {
     }
 
     fn run(self) {
-
+        println!("Listening on {}", self.addr);
     }
 }
+
+struct Request {
+    path: String,
+    query_string: String,
+    // use enums here instead of string
+    method: Method,
+}
+
+// will be represented as ints in memory
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+
+
+/*
+Example request
+
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
