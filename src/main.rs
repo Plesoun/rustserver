7bbb839f61f86279c1ef5ff42ffd62c3e2f1a298
Jakub Schier <jakub.schier@gmail.com>
2021-02-22 23:30:42 +0100

    implement serving contents of a file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use server::Server;
 use website_handler::WebsiteHandler;
+use std::env;
 
 mod server;
 mod http;
 mod website_handler;
 
 fn main() {
-
+	// run with ROOT=$(pwd)/root cargo run or use default
+	let default_path = format!("{}/root", env!("CARGO_MANIFEST_DIR"));
+	let public_path = env::var("ROOT").unwrap_or(default_path);
+	println!("Current path is {}", public_path);
     let server = Server::new("127.0.0.1:8080".to_string()); //need to convert to_string, was a literal
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
 
 
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,34 @@
 use super::server::Handler;
-use super::http::{StatusCode, Request, Response};
+use super::http::{StatusCode, Request, Response, Method};
+use std::fs;
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+	public_path: String
+}
+
+impl WebsiteHandler {
+	pub fn new(public_path: String) -> Self {
+		Self { public_path }
+	}
+	fn read_file(&self, file_path: &str) -> Option<String> {
+		let path = format!("{}/{}", self.public_path, file_path);
+		fs::read_to_string(path).ok()
+	}
+}
 
 impl Handler for WebsiteHandler {
 	fn handle_request(&mut self, request: &Request) -> Response {
-		Response::new(StatusCode::Ok, Some("<h1> Phewphew!! </h1>".to_string()))
+		match request.method() {
+			Method::GET => match request.path() {
+				"/" => Response::new(StatusCode::Ok,
+					self.read_file("index.html")
+				),
+				"/some-weird-stuff-happens-here" => Response::new(StatusCode::Ok,
+					Some("<h1>You are deep in some weird stuff</h1>".to_string())
+				),
+				_ => Response::new(StatusCode::NotFound, None)
+			}
+			_ => Response::new(StatusCode::NotFound, None),
+		}
 	}
 }
