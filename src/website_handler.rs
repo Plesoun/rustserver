16cea80442de9c4aada2a5da72884fbfec8c69f8
Jakub Schier <jakub.schier@gmail.com>
2021-02-22 23:55:26 +0100

    improve upon the file serving

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -12,7 +12,18 @@ impl WebsiteHandler {
 	}
 	fn read_file(&self, file_path: &str) -> Option<String> {
 		let path = format!("{}/{}", self.public_path, file_path);
-		fs::read_to_string(path).ok()
+		match fs::canonicalize(path) {
+			Ok(path) => {
+				if path.starts_with(&self.public_path) {
+				fs::read_to_string(path).ok()
+			} else {
+				println!("Git lost yar! {}", file_path);
+				None
+			}
+		}
+			Err(_) => None,
+		}
+
 	}
 }
 
@@ -26,7 +37,10 @@ impl Handler for WebsiteHandler {
 				"/some-weird-stuff-happens-here" => Response::new(StatusCode::Ok,
 					Some("<h1>You are deep in some weird stuff</h1>".to_string())
 				),
-				_ => Response::new(StatusCode::NotFound, None)
+				path => match self.read_file(path) {
+					Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+					None => Response::new(StatusCode::NotFound, None),
+				}
 			}
 			_ => Response::new(StatusCode::NotFound, None),
 		}
